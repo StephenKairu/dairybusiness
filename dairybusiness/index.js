1f61b03d849df2c7837b41a67ddbfc643a10440f
Stephen Kairu <skairu09@gmail.com>
2022-07-18 02:35:02 +0300

    Add function called totallyWeeklyProduction into javascript

---
+++ dairybusiness/index.js
@@ -0,0 +1,22 @@
+
+let shedA=510;
+let shedB=308;
+let shedC=486;
+let shedD=572;
+ let week=7;   
+let totalDaily= shedA + shedB + shedC + shedD;
+
+function totalWeeklyProduction(totalDaily, week){
+
+    
+    let result = totalDaily * week;
+    return result;
+    
+    }
+    
+let result = totalWeeklyProduction(totalDaily, week)
+console.log(totalDaily)
+console.log(result);
+
+
+
