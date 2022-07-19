a31aab2560ec202cca6711dcb26cb92a00a24ff0
StephenKairu <108527087+StephenKairu@users.noreply.github.com>
2022-07-20 00:46:50 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+
+let shedA=510;
+let shedB=308;
+let shedC=486;
+let shedD=572;
+ let week=7;   
+let totalDaily= shedA + shedB + shedC + shedD;
+
+function totalWeeklyProduction(totalDaily, week){
+
+    
+    let result = totalDaily * week;
+    return result;
+    
+    }
+    
+let result = totalWeeklyProduction(totalDaily, week)
+console.log(totalDaily)
+console.log(result);
+
+let selling_price = 45;
+function incomeOverTime(selling_price, time) {
+    var time = prompt("Weekly");
+    var time = prompt("Yearly")
+
+}
+
+
+let january = 31;
+let february = 29;
+let march = 31;
+let april = 30;
+let may = 31;
+let june = 30;
+let july = 31;
+let august = 31;
+let september = 30;
+let october = 31;
+let november = 30;
+let december = 31;
+
+function monthlyIncome() {
+
+    let januaryIncome= january*totalDaily;
+    let februaryIncome= february*totalDaily;
+    let marchIncome= march*totalDaily;
+    let aprilIncome= april*totalDaily;
+    let mayIncome= may*totalDaily;
+    let juneIncome= june*totalDaily;
+    let julyIncome= july*totalDaily;
+    let augustIncome= august*totalDaily;
+    let septemberIncome= september*totalDaily;
+    let octoberIncome= october*totalDaily;
+    let novemberIncome= november*totalDaily;
+    let decemberIncome= december*totalDaily;
+
+    console.log("Your income for January is " + januaryIncome\n + "Your income for February is" +februaryIncome\n+ "Your income for march"
+    )
+}
